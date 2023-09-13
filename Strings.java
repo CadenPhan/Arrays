57563ec64c5b21bd27d42856cb06ea87f2f9d0e7
Caden Phan <caden.phan@mrjaffesclass.com>
2023-09-13 10:53:04 -0700

    Commit message

--- Strings.java
+++ Strings.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class  Operators
+public class  Strings
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
