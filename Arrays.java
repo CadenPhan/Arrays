4f634f8f6a7b932eb456f0f9428ad87c5cc14d31
Caden Phan <caden.phan@mrjaffesclass.com>
2023-09-15 11:43:38 -0700

    Commit message

--- Arrays.java
+++ Arrays.java
@@ -12,6 +12,7 @@ public class  Arrays
         int i;
         double f = 2.5;
         boolean flag = true;
+        System.out.println(" ");
         i = 10;
         f += i;
         flag = !flag;
@@ -19,6 +20,7 @@ public class  Arrays
         System.out.println(i);
         System.out.println(f);
         System.out.println(flag);
+        System.out.println(" ");
         String str1 = "I am a string";
         String str2 = str1;
         String str3 = "I am also a string";
@@ -27,14 +29,20 @@ public class  Arrays
         System.out.println(str2);
         System.out.println(str3);
         System.out.println(stringsEqual);
+        System.out.println(" ");
         String[] strArray = {"Alpha","Beta","Charlie","Delta","Echo"};
         for (int iterate = 0; iterate < strArray.length; iterate++) {
             System.out.println(strArray[iterate]);
         }
-        System.out.println(" ");
-        for (String value : strArray) {
-            System.out.print(strArray);
-            
+        for (int iterate = 0; iterate < strArray.length; iterate++) {
+            System.out.print(strArray[iterate] + " ");
         }
-    }
-}
+            System.out.println(" ");
+        for (int iterate = 0; iterate < strArray.length; iterate++) {
+            System.out.print(strArray[iterate]);
+            if (iterate < strArray.length - 1) {
+                System.out.print("-");
+            }
+        }
+    } 
+}   
